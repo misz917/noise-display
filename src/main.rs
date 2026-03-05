34484d989973cb43dc0a35a233ec5c3942542358
Milosz <m.szydlowski2003@gmail.com>
2026-03-06 00:05:56 +0100

    working binarization

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
+use crate::{
+    cli::Args,
+    color::{BLACK, WHITE},
+};
 use clap::Parser;
+use image::DynamicImage;
 use minifb::{Key, KeyRepeat, Scale, Window, WindowOptions};
 use rayon::prelude::*;
-
-use crate::cli::Args;
+use std::{path::PathBuf, str::FromStr};
 
 pub mod cli;
+pub mod color;
 
 const BUFFER_WIDTH: usize = 160;
 const BUFFER_HEIGHT: usize = 90;
+const FPS: usize = 20;
+const BINARIZATION_THRESHOLD: u8 = 127;
+
+pub trait IntoBinary {
+    fn binarize(&mut self, threshold: u8) -> Result<(), ()>;
+}
+
+impl IntoBinary for DynamicImage {
+    fn binarize(&mut self, threshold: u8) -> Result<(), ()> {
+        let mut gray = self.to_luma8();
+        let _ = gray.iter_mut().par_bridge().for_each(|pixel| {
+            if *pixel > threshold {
+                *pixel = WHITE as u8;
+            } else {
+                *pixel = BLACK as u8;
+            }
+        });
+
+        *self = DynamicImage::ImageLuma8(gray);
+        Ok(())
+    }
+}
 
 fn main() {
     // let args = Args::parse();
+    let file_path = PathBuf::from_str("hello.png").unwrap();
+    let mut image = image::open(file_path).unwrap();
+    println!("{:?}", image.binarize(BINARIZATION_THRESHOLD));
 
     let mut window = Window::new(
         "ESC to exit; E to pause; R to resume",
@@ -23,7 +53,7 @@ fn main() {
     )
     .unwrap();
 
-    window.set_target_fps(20);
+    window.set_target_fps(FPS);
 
     let mut screen_buffer = [0u32; BUFFER_WIDTH * BUFFER_HEIGHT];
 
@@ -41,11 +71,7 @@ fn main() {
 
         screen_buffer.par_iter_mut().for_each(|pixel| {
             if rand::random_bool(0.5) {
-                let red = 255 << 16;
-                let green = 255 << 8;
-                let blue = 255;
-                let color = red | green | blue;
-                *pixel = color;
+                *pixel = WHITE;
             } else {
                 *pixel = 0;
             }
