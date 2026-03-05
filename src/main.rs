db7e066f395fe0adfb2bb9ef618fb80c972cafaf
Milosz <m.szydlowski2003@gmail.com>
2026-03-05 22:58:30 +0100

    scaling taken care of

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use minifb::{Key, KeyRepeat, Scale, Window, WindowOptions};
+use rayon::prelude::*;
 
 use crate::cli::Args;
 
@@ -8,26 +9,23 @@ pub mod cli;
 const BUFFER_WIDTH: usize = 160;
 const BUFFER_HEIGHT: usize = 90;
 
-const SCREEN_WIDTH: usize = BUFFER_WIDTH * 3;
-const SCREEN_HEIGHT: usize = BUFFER_HEIGHT * 3;
-
 fn main() {
     // let args = Args::parse();
 
     let mut window = Window::new(
-        "ESC to exit; SPACE to pause",
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
+        "ESC to exit; E to pause; R to resume",
+        BUFFER_WIDTH,
+        BUFFER_HEIGHT,
         WindowOptions {
+            scale: Scale::X8,
             ..WindowOptions::default()
         },
     )
     .unwrap();
 
-    window.set_target_fps(60);
+    window.set_target_fps(20);
 
-    let mut small_buffer = [0u32; BUFFER_WIDTH * BUFFER_HEIGHT];
-    let mut screen_buffer = [0u32; SCREEN_WIDTH * SCREEN_HEIGHT];
+    let mut screen_buffer = [0u32; BUFFER_WIDTH * BUFFER_HEIGHT];
 
     let mut paused = false;
     while window.is_open() && !window.is_key_down(Key::Escape) {
@@ -41,7 +39,7 @@ fn main() {
             continue;
         }
 
-        for pixel in screen_buffer.iter_mut() {
+        screen_buffer.par_iter_mut().for_each(|pixel| {
             if rand::random_bool(0.5) {
                 let red = 255 << 16;
                 let green = 255 << 8;
@@ -51,10 +49,10 @@ fn main() {
             } else {
                 *pixel = 0;
             }
-        }
+        });
 
         window
-            .update_with_buffer(&screen_buffer, SCREEN_WIDTH, SCREEN_HEIGHT)
+            .update_with_buffer(&screen_buffer, BUFFER_WIDTH, BUFFER_HEIGHT)
             .unwrap();
     }
 }
