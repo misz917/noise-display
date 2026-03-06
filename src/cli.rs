4e3cb6fe209f3562b7971574e0ad7aecae6ce6e3
mil <m.szydlowski2003@gmail.com>
2026-03-06 12:41:47 +0100

    cli improvements and negative option

--- src/cli.rs
+++ src/cli.rs
@@ -3,18 +3,8 @@ use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(group(
-    clap::ArgGroup::new("input")
-        .required(true)
-        .args(["image", "video", "text"])
-))]
 pub struct Args {
-    #[arg(long)]
-    pub image: Option<PathBuf>,
-
-    #[arg(long)]
-    pub video: Option<PathBuf>,
-
-    #[arg(long)]
-    pub text: Option<String>,
+    pub path: Option<PathBuf>,
+    #[arg(short, long, default_value_t = false)]
+    pub negative: bool,
 }
