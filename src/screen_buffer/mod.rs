9e4253e7026173bedac195ca0fc346473d2dd8b5
mil <m.szydlowski2003@gmail.com>
2026-03-06 15:00:08 +0100

    screen buffer

---
+++ src/screen_buffer/mod.rs
@@ -0,0 +1,36 @@
+pub struct ScreenBuffer {
+    buffer: Vec<u32>,
+    width: usize,
+    height: usize,
+}
+
+impl ScreenBuffer {
+    #[inline]
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            buffer: vec![0; width * height],
+            width,
+            height,
+        }
+    }
+
+    #[inline]
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    #[inline]
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    #[inline]
+    pub fn get_buffer(&self) -> &[u32] {
+        &self.buffer
+    }
+
+    #[inline]
+    pub fn get_buffer_mut(&mut self) -> &mut [u32] {
+        &mut self.buffer
+    }
+}
