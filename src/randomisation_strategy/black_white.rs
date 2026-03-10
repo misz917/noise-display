3a89391347ec4b66a39e65796c848968481c24ad
mil <m.szydlowski2003@gmail.com>
2026-03-10 09:45:54 +0100

    randomisation strategies no longer create vectors at the start when none

--- src/randomisation_strategy/black_white.rs
+++ src/randomisation_strategy/black_white.rs
@@ -10,17 +10,12 @@ pub struct BlackWhiteStrategy;
 impl RandomisationStrategy for BlackWhiteStrategy {
     /// mask: if true => change the pixel; if false => leave unchanged
     fn randomise(&self, buffer: &mut ScreenBuffer, mask: Option<&[bool]>) {
-        let mask = match mask {
-            Some(mask) => mask,
-            None => &vec![true; buffer.height() * buffer.width()],
-        };
-
         buffer
             .get_buffer_mut()
             .par_iter_mut()
             .enumerate()
             .for_each(|(i, pixel)| {
-                if mask[i] {
+                if mask.is_some() && mask.unwrap()[i] {
                     if rand::random_bool(0.5) {
                         *pixel = BLACK;
                     } else {
--- src/randomisation_strategy/rainbow.rs
+++ src/randomisation_strategy/rainbow.rs
@@ -6,17 +6,12 @@ pub struct RainbowStrategy;
 impl RandomisationStrategy for RainbowStrategy {
     /// mask: if true => change the pixel; if false => leave unchanged
     fn randomise(&self, buffer: &mut ScreenBuffer, mask: Option<&[bool]>) {
-        let mask = match mask {
-            Some(mask) => mask,
-            None => &vec![true; buffer.height() * buffer.width()],
-        };
-
         buffer
             .get_buffer_mut()
             .par_iter_mut()
             .enumerate()
             .for_each(|(i, pixel)| {
-                if mask[i] {
+                if mask.is_some() && mask.unwrap()[i] {
                     *pixel = rand::random_range(0..=0xFFFFFF);
                 }
             });
