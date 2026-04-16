d169ddf88088d09aa60836d0b26965ffa46b8071
mil <m.szydlowski2003@gmail.com>
2026-04-16 15:08:50 +0200

    unused constants removed

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,6 @@ pub mod screen_buffer;
 
 pub mod noise_display;
 
-const SCALE: usize = 20;
-
-const BASE_DIMENSIONS: (usize, usize) = (16, 9);
-
-pub const DEFAULT_WIDTH: usize = BASE_DIMENSIONS.0 * SCALE;
-pub const DEFAULT_HEIGHT: usize = BASE_DIMENSIONS.1 * SCALE;
-
 const DEFAULT_TARGET_FPS: usize = 30;
 const BINARIZATION_THRESHOLD: u8 = 127;
 
