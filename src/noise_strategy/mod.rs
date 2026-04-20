0f3608612e901166a96a8fc1bcde68e95622fec2
mil <m.szydlowski2003@gmail.com>
2026-04-20 18:23:03 +0200

    default strategy

--- src/noise_strategy/mod.rs
+++ src/noise_strategy/mod.rs
@@ -1,4 +1,4 @@
-use crate::screen_buffer::ScreenBuffer;
+use crate::{noise_strategy::black_white::BlackWhiteStrategy, screen_buffer::ScreenBuffer};
 
 pub mod black_white;
 pub mod rainbow;
@@ -7,4 +7,10 @@ pub mod slide;
 pub trait NoiseStrategy {
     fn init(&self, buffer: &mut ScreenBuffer);
     fn randomise(&self, buffer: &mut ScreenBuffer, mask: Option<&[bool]>);
+}
+
+impl Default for Box<dyn NoiseStrategy> {
+    fn default() -> Self {
+        Box::new(BlackWhiteStrategy)
+    }
 }
